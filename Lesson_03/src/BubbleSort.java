13c55794e625a7a201244b26bae028b928ce7258
Lập trình cùng Eri <erichannel96@gmail.com>
2022-10-05 21:09:04 +0700

    buổi 3

---
+++ Lesson_03/src/BubbleSort.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static int[] sort(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n - i - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+//            System.out.println("Lần lặp " + (i+1));
+//            System.out.println(Arrays.toString(arr));
+        }
+        return arr;
+    }
+
+    public static int[] sortArray(int[] arr){
+        boolean sorted = false;
+        while (!sorted){
+            sorted = true;
+            for(int i = 0; i < arr.length-1; i++){
+                if(arr[i] > arr[i+1]){
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    sorted = false;
+                }
+            }
+            //System.out.println(Arrays.toString(arr));
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,2,1,5,3};
+        long start = System.nanoTime();
+        int[] sortArr = sort(arr);
+        long end = System.nanoTime();
+
+        System.out.println(end - start);
+        System.out.println(Arrays.toString(sortArr));
+    }
+}
---
+++ Lesson_03/src/_414.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class _414 {
+    public int thirdMax(int[] nums) {
+        int n = nums.length;
+        if(n == 1){
+            return nums[0];
+        }else if(n == 2){
+            return Math.max(nums[0], nums[1]);
+        }else{
+            Arrays.sort(nums);
+        }
+
+    }
+}
